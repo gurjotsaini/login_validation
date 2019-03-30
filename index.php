1e1cbea9369f98b193b46b7533fb289a2716325c
gurjotsaini <gurjotsaini1992@gmail.com>
2019-03-30 23:00:15 +0530

    Added: Root files

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php require "includes/header.php"; ?>
+<?php require "includes/nav.php"; ?>
+<div class="jumbotron">
+    <h1 class="text-center"> Home Page</h1>
+</div>
+<?php require "includes/footer.php"; ?>

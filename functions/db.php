5b44b9f4e04471870b0d47a4a10edeceb0f1da3d
gurjotsaini <gurjotsaini1992@gmail.com>
2019-03-30 23:21:51 +0530

    Added: more essential files & directories for future use

---
+++ functions/db.php
@@ -0,0 +1,6 @@
+<?php
+    /**
+     * Created by User: gurjot
+     */
+
+    $con = mysqli_connect('localhost', 'root','root', 'login_validation');
--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,4 @@
+<?php require "functions/init.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>

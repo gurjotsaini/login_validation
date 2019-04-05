fc90c2ee7d27c739e1e9f440e9f6cb30a1f11775
gurjotsaini <gurjotsaini1992@gmail.com>
2019-04-05 14:36:43 +0530

    Added: class & method for sending mail

---
+++ core/MailClass/Mail.php
@@ -0,0 +1,35 @@
+<?php
+    /**
+     * Created by User: gurjot
+     */
+
+    namespace App\Core\MailClass\Mail;
+    use PHPMailer\PHPMailer\PHPMailer;
+    use App\Core\Config\Config;
+
+    class Mail
+    {
+        private static function sendMail($email = null, $subject = null, $message = null, $headers = null) {
+            $mail = new PHPMailer();
+
+            //Server settings
+            $mail->SMTPDebug = 2;                                  // Enable verbose debug output
+            $mail->isSMTP();                                       // Set mailer to use SMTP
+            $mail->Host       = Config::SMTP_HOST;                 // Specify main and backup SMTP servers
+            $mail->SMTPAuth   = true;                              // Enable SMTP authentication
+            $mail->Username   = Config::SMTP_USERNAME;             // SMTP username
+            $mail->Password   = Config::SMTP_PASSWORD;             // SMTP password
+            $mail->SMTPSecure = Config::SMTP_ENCRYPTION;           // Enable TLS encryption, `ssl` also accepted
+            $mail->Port       = Config::SMTP_PORT;                 // TCP port to connect to
+
+            // Content
+            $mail->isHTML(true);                             // Set email format to HTML
+            $mail->Subject = $subject;
+            $mail->Body    = $message;
+            $mail->AltBody = $message;
+
+            if (!$mail->send()) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
+        }
+    }
